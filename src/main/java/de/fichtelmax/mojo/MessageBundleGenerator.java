4cff789cf07a7d87f3d6fb58eea355621bd091e7
Max Fichtelmann <max.fichtelmann@gmail.com>
2016-01-28 14:10:36 +0100

    HelloWorld style mojo

---
+++ src/main/java/de/fichtelmax/mojo/MessageBundleGenerator.java
@@ -0,0 +1,17 @@
+package de.fichtelmax.mojo;
+
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.MojoFailureException;
+import org.apache.maven.plugins.annotations.LifecyclePhase;
+import org.apache.maven.plugins.annotations.Mojo;
+
+@Mojo(name = "generate", defaultPhase = LifecyclePhase.GENERATE_SOURCES)
+public class MessageBundleGenerator extends AbstractMojo {
+
+	@Override
+	public void execute() throws MojoExecutionException, MojoFailureException {
+		getLog().info("Hello World!");
+	}
+
+}
