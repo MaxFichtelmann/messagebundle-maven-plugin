edad8cac09d95fd768bf6a14e49292f48d1e23c5
Max Fichtelmann <max.fichtelmann@gmail.com>
2016-04-04 22:24:31 +0200

    retain property name

--- src/main/java/de/fichtelmax/mojo/messagebundle/generate/EnumGenerator.java
+++ src/main/java/de/fichtelmax/mojo/messagebundle/generate/EnumGenerator.java
@@ -3,11 +3,17 @@ package de.fichtelmax.mojo.messagebundle.generate;
 import org.apache.commons.lang3.StringUtils;
 
 import com.sun.codemodel.ClassType;
+import com.sun.codemodel.JBlock;
 import com.sun.codemodel.JClassAlreadyExistsException;
 import com.sun.codemodel.JCodeModel;
 import com.sun.codemodel.JDefinedClass;
+import com.sun.codemodel.JEnumConstant;
+import com.sun.codemodel.JExpr;
+import com.sun.codemodel.JFieldVar;
+import com.sun.codemodel.JMethod;
 import com.sun.codemodel.JMod;
 import com.sun.codemodel.JPackage;
+import com.sun.codemodel.JVar;
 
 import de.fichtelmax.mojo.messagebundle.model.MessageBundleInfo;
 import de.fichtelmax.mojo.messagebundle.model.MessagePropertyInfo;
@@ -16,22 +22,47 @@ public class EnumGenerator {
 
 	public void transformToEnumInfo(MessageBundleInfo info, JCodeModel codeModel) {
 		JPackage _package;
-		
+
 		if (StringUtils.isNotBlank(info.getPackageName())) {
 			_package = codeModel._package(info.getPackageName());
 		} else {
 			_package = codeModel.rootPackage();
 		}
-		
+
 		try {
 			JDefinedClass _class = _package._class(JMod.PUBLIC, info.getName(), ClassType.ENUM);
-			
-			for (MessagePropertyInfo propertyInfo: info.getPropertyInfos()) {
-				String enumConstantName = propertyInfo.getPropertyName().replaceAll("[^a-zA-Z0-9_]", "_").toUpperCase();
-				_class.enumConstant(enumConstantName);
+
+			JFieldVar propertyNameField = _class.field(JMod.PRIVATE, String.class, "propertyName");
+			generateConstructor(_class, propertyNameField);
+			generatePropertyNameGetter(_class, propertyNameField);
+
+			if (null == info.getPropertyInfos() || info.getPropertyInfos().isEmpty()) {
+				generateEnumConstant(_class, "DUMMY");
+			}
+
+			for (MessagePropertyInfo propertyInfo : info.getPropertyInfos()) {
+				generateEnumConstant(_class, propertyInfo.getPropertyName());
 			}
 		} catch (JClassAlreadyExistsException e) {
 			throw new RuntimeException(e);
 		}
+	}
+
+	private void generateEnumConstant(JDefinedClass _class, String propertyName) {
+		String enumConstantName = propertyName.replaceAll("[^a-zA-Z0-9_]", "_").toUpperCase();
+		JEnumConstant enumConstant = _class.enumConstant(enumConstantName);
+		enumConstant.arg(JExpr.lit(propertyName));
+	}
+
+	private void generatePropertyNameGetter(JDefinedClass _class, JFieldVar propertyNameField) {
+		JMethod propertyNameGetter = _class.method(JMod.PUBLIC, String.class, "getPropertyName");
+		propertyNameGetter.body()._return(propertyNameField);
+	}
+
+	private void generateConstructor(JDefinedClass _class, JFieldVar nameField) {
+		JMethod constructor = _class.constructor(JMod.PRIVATE);
+		JVar param = constructor.param(String.class, "propertyName");
+		JBlock body = constructor.body();
+		body.assign(JExpr._this().ref(nameField), param);
 	}
 }
--- src/test/java/de/fichtelmax/mojo/messagebundle/generate/EnumGeneratorTest.java
+++ src/test/java/de/fichtelmax/mojo/messagebundle/generate/EnumGeneratorTest.java
@@ -1,5 +1,6 @@
 package de.fichtelmax.mojo.messagebundle.generate;
 
+import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.CoreMatchers.instanceOf;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.Matchers.arrayWithSize;
@@ -7,6 +8,7 @@ import static org.junit.Assert.assertThat;
 
 import java.io.File;
 import java.io.IOException;
+import java.lang.reflect.Method;
 import java.util.Collections;
 
 import org.codehaus.plexus.util.FileUtils;
@@ -22,79 +24,106 @@ import de.fichtelmax.mojo.messagebundle.util.Compiler;
 
 public class EnumGeneratorTest {
 
+	private static final String SOME_CLASS_NAME = "SomeClass";
+
 	EnumGenerator cut = new EnumGenerator();
 
 	File dir;
-	
+
 	@Before
 	public void createDir() throws IOException {
 		dir = File.createTempFile("dir", "");
 		dir.delete();
 		dir.mkdir();
 	}
-	
+
 	@After
 	public void removeDir() throws IOException {
 		FileUtils.deleteDirectory(dir);
 	}
-	
+
 	@Test
 	public void classInfoShouldBeGenerated() throws Exception {
 		String name = "Foo";
 
 		MessageBundleInfo info = new MessageBundleInfo();
 		info.setName(name);
-		
+
 		JCodeModel codeModel = new JCodeModel();
 		cut.transformToEnumInfo(info, codeModel);
-		
+
 		codeModel.build(dir);
 		Compiler.compile(name, dir);
 		Class<?> fooClass = Compiler.loadClass(name, dir);
-		
+
 		assertThat(fooClass.isEnum(), is(true));
 	}
-	
+
 	@Test
 	public void qualifiedClassInfoShouldBeGenerated() throws Exception {
 		String name = "Bar";
 		String packageName = "foo";
 
 		String fullName = "foo.Bar";
-		
+
 		MessageBundleInfo info = new MessageBundleInfo();
 		info.setName(name);
 		info.setPackageName(packageName);
-		
+
 		JCodeModel codeModel = new JCodeModel();
 		cut.transformToEnumInfo(info, codeModel);
-		
+
 		codeModel.build(dir);
 		Compiler.compile(fullName, dir);
 		Class<?> barClass = Compiler.loadClass(fullName, dir);
-		
+
 		assertThat(barClass.isEnum(), is(true));
 	}
-	
+
 	@Test
 	public void enumConstantShouldBeGenerated() throws Exception {
-		String name = "Bar";
+		String name = SOME_CLASS_NAME;
 		String propertyName = "baz";
-		
+
 		MessageBundleInfo info = new MessageBundleInfo();
 		info.setName(name);
 		MessagePropertyInfo propertyInfo = new MessagePropertyInfo();
 		propertyInfo.setPropertyName(propertyName);
 		info.setPropertyInfos(Collections.singleton(propertyInfo));
-		
+
 		JCodeModel codeModel = new JCodeModel();
 		cut.transformToEnumInfo(info, codeModel);
-		
+
 		codeModel.build(dir);
 		Compiler.compile(name, dir);
 		Class<?> barClass = Compiler.loadClass(name, dir);
-		
+
 		assertThat(barClass.getEnumConstants(), is(arrayWithSize(1)));
 		assertThat(barClass.getEnumConstants()[0], is(instanceOf(Enum.class)));
+	}
+
+	@Test
+	public void enumShouldRetainOriginalPropertyName() throws Exception {
+		String name = SOME_CLASS_NAME;
+		String propertyName = "foo.bar";
+
+		MessageBundleInfo info = new MessageBundleInfo();
+		info.setName(name);
+		MessagePropertyInfo propertyInfo = new MessagePropertyInfo();
+		propertyInfo.setPropertyName(propertyName);
+		info.setPropertyInfos(Collections.singleton(propertyInfo));
+
+		JCodeModel codeModel = new JCodeModel();
+		cut.transformToEnumInfo(info, codeModel);
+
+		codeModel.build(dir);
+		Compiler.compile(name, dir);
+		Class<?> barClass = Compiler.loadClass(name, dir);
+
+		Object constant = barClass.getEnumConstants()[0];
+		Method getter = barClass.getMethod("getPropertyName");
+
+		String obtainedPropertyName = (String) getter.invoke(constant);
+		assertThat(obtainedPropertyName, is(equalTo(propertyName)));
 	}
 }
